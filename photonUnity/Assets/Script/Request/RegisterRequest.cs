d1d18842510cd01a2ee2e3ac39ce2c54a554b952
book <297982067@qq.com>
2019-04-03 16:49:51 +0800

    移除不必要文件

---
+++ photonUnity/Assets/Script/Request/RegisterRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ExitGames.Client.Photon;
+using UnityEngine;
+using Common;
+
+public class RegisterRequest : Request
+{
+    [HideInInspector]
+    public string username;
+    [HideInInspector]
+    public string password;
+
+    public override void DefaultRequest()
+    {
+        Dictionary<byte, object> data = new Dictionary<byte, object>();
+        data.Add((byte)ParameterCode.Name, username);
+        data.Add((byte)ParameterCode.Pwd, password);
+        PhotonEngine.Peer.OpCustom((byte)opCode, data, true);
+    }
+
+    public override void OnOperationResponse(OperationResponse operationResponse)
+    {
+        throw new System.NotImplementedException();
+    }
+}
